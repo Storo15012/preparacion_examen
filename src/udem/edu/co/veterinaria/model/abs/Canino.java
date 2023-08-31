93eb5a3ef4cafed3344ca55bca410f4816bf4b1c
Storo15012 <samueltoroangarita15012@gmail.com>
2023-08-30 20:30:15 -0500

    main actualizado

--- src/udem/edu/co/veterinaria/model/abs/Canino.java
+++ src/udem/edu/co/veterinaria/model/abs/Canino.java
@@ -2,7 +2,7 @@ package udem.edu.co.veterinaria.model.abs;
 
 import udem.edu.co.veterinaria.model.interfaces.Animal;
 
-public class Canino implements Animal {
+public abstract class Canino implements Animal {
     @Override
     public void mover() {
 
--- src/udem/edu/co/veterinaria/model/abs/Felino.java
+++ src/udem/edu/co/veterinaria/model/abs/Felino.java
@@ -2,7 +2,7 @@ package udem.edu.co.veterinaria.model.abs;
 
 import udem.edu.co.veterinaria.model.interfaces.Animal;
 
-public class Felino  implements Animal {
+public abstract class Felino  implements Animal {
     @Override
     public void mover() {
 
